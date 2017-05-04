1cdb7394f43a06bc5623878f880dcc291efef7e1
Imma Cabanes <imma.cabanes@iesabastos.org>
2017-05-04 23:14:57 +0200

    primer commit

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Mi primer programa
+ * 
+ */
+
+/**
+ *
+ * @author DAM
+ */
+public class HolaMundo {
+    public static void main (String[] args){
+        System.out.println("Hola, mundo!");
+       // int i;
+        
+    }
+}
